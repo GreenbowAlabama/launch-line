ca48276415c15184c0a96060e466ac63398608b0
ivncvrter <164603028+ivncvrter@users.noreply.github.com>
2025-05-22 21:20:47 -0500

    51

--- ui/src/components/Dashboard.jsx
+++ ui/src/components/Dashboard.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import TestKickUploader from "./TestKickUploader";
 
 export default function Dashboard() {
-  const { user, logout } = useContext(AuthContext);
+  const { logout } = useContext(AuthContext);
 
   return (
     <div className="p-6 text-white">
